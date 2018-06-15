c75a042e7a808016fc9f2c89321395272c9f82e2
Dóra Surmann <surmann.dora@gmail.com>
2018-06-15 13:14:46 +0200

    Second route implemented Put method argument into webroute annotation

--- src/com/codecool/surmann/webroute/Router.java
+++ src/com/codecool/surmann/webroute/Router.java
@@ -3,14 +3,25 @@ package com.codecool.surmann.webroute;
 import com.sun.net.httpserver.HttpExchange;
 
 public class Router {
+    private static int counter = 0;
 
-    @WebRoute(path = "/")
+    @WebRoute(path = "/", method = WebRoute.HTTPMethod.GET)
     public static String Homepage(HttpExchange exchange) {
 
         return "<!doctype html>" +
                 "<html lang=\"en\">" +
                 "<head><title>Main page</title></head>" +
-                "<body><h1>Welcome!</h1></body>" +
+                "<body><h1>Welcome! " + ++counter +
+                "</h1></body>" +
+                "</html>";
+    }
+    @WebRoute(path = "/other")
+    public static String Otherpage(HttpExchange exchange) {
+
+        return "<!doctype html>" +
+                "<html lang=\"en\">" +
+                "<head><title>Main page</title></head>" +
+                "<body><h2>some other page</h2></body>" +
                 "</html>";
     }
 }
