93277b3c4f50f1a25fdeb4757a940685632155ad
Dóra Surmann <surmann.dora@gmail.com>
2018-06-15 13:15:35 +0200

    Refactoring:deleted unused lines

--- src/com/codecool/surmann/webroute/Server.java
+++ src/com/codecool/surmann/webroute/Server.java
@@ -5,10 +5,7 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class Server {
     private static HttpServer server1;
@@ -28,24 +25,11 @@ public class Server {
         for (Method method : Router.class.getDeclaredMethods()) {
             if (method.isAnnotationPresent(WebRoute.class)) {
                 String path = method.getAnnotation(WebRoute.class).path();
-                List<String> params = paramsFromPath(path);
                 Server.server1.createContext(path, new Handler(method));
-                System.out.println("    route to path \"" + path + "\" set up");
+                System.out.println("    route \"" + path + "\" has set up");
             }
-            System.out.println("Server routes successfully set up");
         }
-    }
-    private static List<String> paramsFromPath(String path) {
-        Pattern pattern1 = Pattern.compile("<.*?>");
-        Matcher matcher1 = pattern1.matcher(path);
-        List<String> params = new ArrayList<>();
-
-        while (matcher1.find()) {
-            String param = matcher1.group(0);
-            params.add(param.substring(1, param.length() - 1));
-        }
-        return params;
-
+        System.out.println("Routes set up finished");
     }
 }
 
